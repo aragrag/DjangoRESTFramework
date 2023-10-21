0a123b18ed82309982a3a31dfe693a322e64cd01
Ayoub <aragrag.ayoub@gmail.com>
2023-10-21 12:52:14 -0300

    Commit Performance - Django

--- shop/views.py
+++ shop/views.py
@@ -1,32 +1,59 @@
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.viewsets import ReadOnlyModelViewSet
-
+from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from shop.models import Category, Product, Article
-from shop.serializers import CategorySerializer, ProductSerializer, ArticleSerializer
+from shop.serializers import CategoryDetailSerializer, CategoryListSerializer,\
+    ProductDetailSerializer, ProductListSerializer, ArticleSerializer
 
 
+class MultipleSerializerMixin:
 
-class CategoryViewset(ReadOnlyModelViewSet):
- 
-    serializer_class = CategorySerializer
- 
+    detail_serializer_class = None
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve' and self.detail_serializer_class is not None:
+            return self.detail_serializer_class
+        return super().get_serializer_class()
+
+
+class AdminCategoryViewset(MultipleSerializerMixin, ModelViewSet):
+
+    serializer_class = CategoryListSerializer
+    detail_serializer_class = CategoryDetailSerializer
+    queryset = Category.objects.all()
+
+
+class CategoryViewset(MultipleSerializerMixin, ReadOnlyModelViewSet):
+
+    serializer_class = CategoryListSerializer
+    detail_serializer_class = CategoryDetailSerializer
+
     def get_queryset(self):
         return Category.objects.filter(active=True)
 
+    @action(detail=True, methods=['post'])
+    def disable(self, request, pk):
+        self.get_object().disable()
+        return Response()
 
-class ProductViewset(ReadOnlyModelViewSet):
- 
-    serializer_class = ProductSerializer
- 
+
+class ProductViewset(MultipleSerializerMixin, ReadOnlyModelViewSet):
+
+    serializer_class = ProductListSerializer
+    detail_serializer_class = ProductDetailSerializer
+
     def get_queryset(self):
-    # Nous récupérons tous les produits dans une variable nommée queryset
         queryset = Product.objects.filter(active=True)
-        # Vérifions la présence du paramètre ‘category_id’ dans l’url et si oui alors appliquons notre filtre
         category_id = self.request.GET.get('category_id')
-        if category_id is not None:
+        if category_id:
             queryset = queryset.filter(category_id=category_id)
         return queryset
+
+    @action(detail=True, methods=['post'])
+    def disable(self, request, pk):
+        self.get_object().disable()
+        return Response()
 
 
 class ArticleViewset(ReadOnlyModelViewSet):
@@ -39,3 +66,9 @@ class ArticleViewset(ReadOnlyModelViewSet):
         if product_id is not None:
             queryset = queryset.filter(product_id=product_id)
         return queryset
+
+
+class AdminArticleViewset(ModelViewSet):
+
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
